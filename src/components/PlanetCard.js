31631692d57570e635539deafcc178808ce4272f
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-02-18 16:22:15 -0300

    Resolvi erros de Lint

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Planetcard extends React.Component {
+  render() {
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">
+          jkk
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Planetcard;
