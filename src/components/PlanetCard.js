2c8ea0d4edb24b84c0a2bb5c8d84744b2aec3d0e
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-02-18 18:44:58 -0300

    Requisito 06 Ok

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -3,10 +3,9 @@ import PropTypes from 'prop-types';
 
 class Planetcard extends React.Component {
   render() {
-    const { planetImage } = this.props;
-    const { planetName } = this.props;
+    const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div className="planetCard" data-testid="planet-card">
         <p data-testid="planet-name">{ planetName }</p>
         <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Title from './Title';
+import Planetcard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { planets.map((planetInfos) => (
+          <Planetcard
+            key={ planetInfos.name }
+            planetName={ planetInfos.name }
+            planetImage={ planetInfos.image }
+          />
+        ))}
       </div>
     );
   }
