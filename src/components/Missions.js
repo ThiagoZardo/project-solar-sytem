9777ba2e413264479c777cd06db528d88c88b11a
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2022-02-18 18:57:04 -0300

    Requisito 08 Ok

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
-      <div className="mission" data-testid="missions" />
+      <div className="mission" data-testid="missions">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }
